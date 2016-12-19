cf7bb2ff78c604e526cff9e95d914c19dbd1fc34
Juan A Cortes <juan.cortes@gmail.com>
2016-12-19 00:53:38 -0500

    Add ability to update clients' information

--- app.js
+++ app.js
@@ -11,20 +11,21 @@ app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
 
 app.get("/clients", function(request, response) {
-  console.log("Request for clients received");
+  // console.log("Request for clients received");
 
   db.clients.find().sort({first_name: 1}, function(error, docs) {
     if (error) {
       response.send(error);
     } else {
-      console.log("Sending data...");
-      console.log(docs);
+      // console.log("Sending data...");
+      // console.log(docs);
       response.json(docs);
     }
   });
 });
 
 app.post("/clients", function(request, response) {
+  // console.log("request sent by the front end", request.body);
   db.clients.insert(request.body, function(error, doc) {
     if (error) {
       response.send(error);
@@ -34,6 +35,46 @@ app.post("/clients", function(request, response) {
       response.json(doc);
     }
   });
+});
+
+app.get("/clients/:id", function(request, response) {
+  var id = request.params.id;
+  console.log("ID of client to edit: ", id);
+
+  db.clients.findOne({
+    _id: mongojs.ObjectId(id)},
+    function(error, doc) {
+    if (error) {
+      response.send(error);
+    } else {
+      response.json(doc);
+    }
+  });
+});
+
+app.put("/clients/:id", function(request, response) {
+  var id = request.params.id;
+
+  db.clients.findAndModify({
+    query: { _id: mongojs.ObjectId(id) },
+    update: {
+      $set: {
+        first_name: request.body.first_name,
+        last_name: request.body.last_name,
+        email: request.body.email,
+        phone: request.body.phone
+      }
+    },
+    new: true
+  }, function (error, doc, lastErrorObject) {
+    console.log("This is the doc that would be updated: ", doc);
+    if (error) {
+      response.send(error);
+    } else {
+      response.json(doc);
+    }
+  });
+
 });
 
 
--- public/controllers/AppController.js
+++ public/controllers/AppController.js
@@ -3,7 +3,7 @@ var myApp = angular.module("myApp", []);
 myApp.controller("AppController", ["$scope", "$http" ,"$location", function($scope, $http, $location) {
   $http.get("/clients")
     .then(function(response) {
-      console.log("Data received from the server", response);
+      // console.log("Data received from the server", response);
       $scope.clients = response.data;
     });
 
@@ -15,4 +15,24 @@ myApp.controller("AppController", ["$scope", "$http" ,"$location", function($sco
         window.location.href = "/";
       });
   };
+
+  $scope.editClient = function(id) {
+    $("#addBtn").remove();
+    $http.get("/clients/" + id)
+      .then(function(response) {
+        $scope.client = response.data;
+      });
+  };
+
+  $scope.updateClient = function() {
+    console.log("This is the client to update: ", $scope.client);
+    // debugger
+    console.log("Logging $scope.client: ", $scope.client);
+    $http.put("/clients/" + $scope.client._id, $scope.client)
+      .then(function(response) {
+        console.log("Client updated after it returns from the backend", response);
+        window.location.href = "/";
+      });
+  };
+
 }]);
